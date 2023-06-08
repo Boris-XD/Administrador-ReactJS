a4801eb90e6baa4234c2e3f7275e6672472f65d8
Boris Sea <seaborisrc@gmail.com>
2023-06-07 22:44:45 -0400

    Add Client Table

---
+++ src/components/ClientTable.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ClientTable = ({ client }) => {
+    const { nombre, empresa, email, telefono, id } = client;
+  return (
+    <tr className='border-b'>
+        <td className='p-6 space-y-2'>
+            <p className="text-2xl text-gray-800">{nombre}</p>
+            <p>{empresa}</p>
+        </td>
+        <td className='p-6'>
+            <p className="text-gray-600"><span className="text-gray-800 font-bold">Email: </span>{email}</p>
+            <p className="text-gray-600"><span className="text-gray-800 font-bold">Tel: </span>{telefono}</p>
+        </td>
+        <td className='p-6 flex gap-3'>
+            <button className="text-blue-600 hover:text-blue-800 font-blod text-xs">
+                Editar
+            </button>
+            <button className="text-red-600 hover:text-red-800 font-blod text-xs">
+                Eliminar
+            </button>
+        </td>
+    </tr>
+  )
+}
+
+export default ClientTable
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom"
+import ClientTable from "../components/ClientTable";
 
 export function loader(){
     return [
@@ -42,9 +43,37 @@ export function loader(){
 
 const Index = () => {
     const clients = useLoaderData()
-    console.log(clients)
+    
   return (
-    <div>Complement of Main Page</div>
+    <>
+        <h1 className="font-black text-4xl text-blue-900">Clients</h1>
+        <p className="mt-3">Manage your clients</p>
+        {
+            clients.length ? (
+                <table className="w-full bg-white shadow mt-5 table-auto">
+                    <thead className="bg-blue-800 text-white">
+                        <tr>
+                            <th className="p-2">Client</th>
+                            <th className="p-2">Contact</th>
+                            <th className="p-2">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            clients.map(client => (
+                                <ClientTable
+                                    client={client}
+                                    key={client.id}
+                                />
+                            ))
+                        }
+                    </tbody>
+                </table>
+            ) : (
+                <p className="text-center mt-10">There isn't clients</p>
+            )
+        }
+    </>
   )
 }
 
